b0abc7ccdc718e180ee44100a6d2f43ef6daf4ee
Jose A. Chavez <joseacb144@gmail.com>
2020-06-10 01:14:39 -0500

    Upload current state of folder.

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+// Set-up used to randomly get a Superhero name.
+var superheroes = require("superheroes");
+var mySuperHeroName = superheroes.random();
+console.log("My Superhero name is: " + mySuperHeroName);
+
+// Set-up used to randomly get a Supervillain name.
+var supervillains = require("supervillains");
+var mySuperVillainName = supervillains.random();
+console.log("My Supervillain name is: " + mySuperVillainName);
